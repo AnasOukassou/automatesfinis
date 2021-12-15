d66eeb1e0fbaefe57d723c8d1fd1730932435282
o20027606 <anas.oukassou@etu.univ-amu.fr>
2021-12-15 17:11:09 +0100

    tp3 done

--- tp3automates.py
+++ tp3automates.py
@@ -9,21 +9,70 @@ import pdb # for debugging
 
 ##################
 
+def new_state(a1:Automaton) -> str:
+    maxstate = -1
+    for a in a1.states:
+        try:
+            maxstate = max(int(a), maxstate)
+        except ValueError:
+            pass
+    return str(maxstate + 1)
+
+
 def kleene(a1:Automaton)->Automaton:
-  # TODO: implement Kleene's star
-  return a1 
+    new_aut = a1.deepcopy()
+    new_aut.name = "kleene(" + a1.name + ")"
+    for accepting_state in a1.acceptstates:
+        new_aut.add_transition(accepting_state, EPSILON, a1.initial.name)
+    new_state_name = new_state(new_aut)
+    new_aut.add_transition(new_state_name, EPSILON, a1.initial.name)
+    new_aut.initial = new_aut.statesdict[new_state_name]
+    new_aut.make_accept(new_state_name)
+    return new_aut 
 
 ##################
 
 def concat(a1:Automaton, a2:Automaton)->Automaton:
-  # TODO: implement concatenation
-  return a1 
+    new_aut = a2.deepcopy()
+    new_aut.name = "concat(" + a1.name + "," + a2.name + ")"
+    new_state_name = str(max(int(new_state(a1)), int(new_state(a2))))
+    for state in a2.states:
+        if state in a1.states or state:
+            new_aut.rename_state(state, new_state_name)
+            new_state_name = str(int(new_state_name) + 1)
+    
+    for (s,a,d) in a1.transitions:
+        new_aut.add_transition(s, a, d)
+    
+    for state in a1.acceptstates:
+        new_aut.add_transition(state, EPSILON, new_aut.initial.name)
+    
+    new_aut.initial = new_aut.statesdict[a1.initial.name]
+    
+    return new_aut
+    
 
 ##################
 
 def union(a1:Automaton, a2:Automaton)->Automaton:
-  # TODO: implement union
-  return a1 
+    new_aut = a2.deepcopy()
+    new_aut.name = "union(" + a1.name + "," + a2.name + ")"
+    new_state_name = str(max(int(new_state(a1)), int(new_state(a2))))
+    for state in a2.states:
+        if state in a1.states:
+            new_aut.rename_state(state, new_state_name)
+            new_state_name = str(int(new_state_name) + 1)
+            
+    for (s,a,d) in a1.transitions:
+        new_aut.add_transition(s, a, d)
+        
+    new_aut.make_accept(a1.acceptstates)
+    
+    new_aut.add_transition(new_state_name, EPSILON, new_aut.initial.name)
+    new_aut.add_transition(new_state_name, EPSILON, a1.initial.name)
+    new_aut.initial = new_aut.statesdict[new_state_name]
+    
+    return new_aut 
 
 ##################
 
